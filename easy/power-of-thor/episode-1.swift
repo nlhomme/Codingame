2f092ecc0c7874643ddcf90c4de452cf75567070
Nicolas Lhomme <nlhomme@users.noreply.github.com>
2020-09-28 17:34:33 +0200

    Update episode-1.swift
    Add comment
    Update X part to be read the same way as Y part

--- easy/power-of-thor/episode-1.swift
+++ easy/power-of-thor/episode-1.swift
@@ -19,38 +19,47 @@ let lightY = Int(inputs[1])! // the Y position of the light of power
 let initialTx = Int(inputs[2])! // Thor's starting X position
 let initialTy = Int(inputs[3])! // Thor's starting Y position
 
+// Current Thor location initialisation
 var positionX = initialTx
 var positionY = initialTy
 
 // game loop
 while true {
 
+// Direction Y and X initalisation
 var directionY = ""
 var directionX = ""
 var directionFinale = ""
+
     let remainingTurns = Int(readLine()!)! // The remaining amount of turns Thor can move. Do not remove this line.
 
     print("Tours restants: \(remainingTurns)", to: &errStream)
-
-    // Write an action using print("message...")
-    // To debug: print("Debug messages...", to: &errStream)
+    
+    // Y axis //
+    // If light is closer to 0 on Y axis
     if lightY < positionY  {
+        //Then Thor must go North
         directionY = "N"
+        // Update Thor location on Y axis
         positionY -= 1
+      // Else if Thor is closer to 0 on Y axis
     } else if lightY > positionY {
+        // Then Thor must go South
         directionY = "S"
         positionY += 1
+      // If Thor and Light are on the same axis value
+      // Thor doesn't need to move
     } else {
         directionY = ""
     }
     
-
-    if lightX > positionX {
-        directionX = "E"
-        positionX += 1
-    } else if lightX < positionX {
+    //X Axis
+    if lightX < positionX {
         directionX = "W"
         positionX -= 1
+    } else if lightX > positionX {
+        directionX = "E"
+        positionX += 1
     } else {
         directionX = ""
     }
