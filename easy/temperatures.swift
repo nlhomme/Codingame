7fd31b6c1ba2288b323bfc80419e20ce11ea13f6
nlhomme <nlhomme@oceanet-technology.com>
2020-10-23 16:44:13 +0200

    Temperatures: challenge completed entirely

--- easy/temperatures.swift
+++ easy/temperatures.swift
@@ -12,33 +12,51 @@ public var errStream = StderrOutputStream()
  **/
 
 var temperatureArray = [Int]()
-var row1 = [Int]()
-var row2 = [Int]()
+var candidate = 999
 var result = 999
 
 let n = Int(readLine()!)! // the number of temperatures to analyse
 for i in ((readLine()!).split(separator: " ").map(String.init)) {
     let t = Int(i)!
 
-    // Store temperatures to an array
+    // Storing temperatures to an array
     temperatureArray.append(t)
-
 }
 
+// If no temperature to analyse
+// Then nothing to do
 if n == 0 {
-    result = 0
+    candidate = 0
 }
 
+// For each temperature in our array
 temperatureArray.forEach {
 
-    print("La difference entre 0 et \($0) est \(abs(0-$0))", to: &errStream)
+    print("The difference between 0 and \($0) is \(abs(0-$0))", to: &errStream)
 
-    if abs(0-$0) < abs(result) {
-        result = $0
-        print("result = \($0)", to: &errStream)
+    // If two array entries 
+    // (one postive and one negative)
+    // are both close to zero then the postitive entry wins
+    if abs(0-$0) == abs(candidate){
+        candidate = abs($0)
+        print("candidate = \($0)", to: &errStream)
     }
+
+    // If the array entry is smaller than the candidate
+    // And the array entry and the candidate are both close to zero then the postitive entry wins
+    if abs(0-$0) < abs(candidate) {
+        if abs(0-$0) == abs(candidate){
+            candidate = abs($0)
+            print("candidate = \($0)", to: &errStream)
+        }
+            // Else the array entry wins
+            candidate = $0
+            print("candidate = \($0)", to: &errStream)
+        }
 }
 
+// The candidate become the final result
+result = candidate
 
 print(result);
 
